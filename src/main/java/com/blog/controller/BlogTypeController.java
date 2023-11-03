72d2fc7bc0fcce1425e41ad0d2ed0bb3936443b7
MrXck <1971877559@qq.com>
2023-11-03 09:40:17 +0800

    完成博客分类接口

---
+++ src/main/java/com/blog/controller/BlogTypeController.java
@@ -0,0 +1,25 @@
+package com.blog.controller;
+
+import com.blog.dto.type.BlogTypeDTO;
+import com.blog.service.BlogTypeService;
+import com.blog.utils.NoAuthorization;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blogType")
+public class BlogTypeController {
+
+    @Autowired
+    private BlogTypeService blogTypeService;
+
+    @GetMapping("/all")
+    @NoAuthorization
+    public BlogTypeDTO all() {
+        BlogTypeDTO blogTypeDTO = new BlogTypeDTO();
+        blogTypeDTO.setBlogTypes(blogTypeService.list());
+        return blogTypeDTO;
+    }
+}
---
+++ src/main/java/com/blog/dto/type/BlogTypeDTO.java
@@ -0,0 +1,13 @@
+package com.blog.dto.type;
+
+import com.blog.pojo.BlogType;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BlogTypeDTO {
+
+    private List<BlogType> blogTypes;
+
+}
